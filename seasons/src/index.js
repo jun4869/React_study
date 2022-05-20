6dc1a41821fba051ebae2337c747e3a9f3dcf03b
jun4869 <jun2194k@gmail.com>
2022-05-20 16:10:54 +0900

    section5

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,18 +2,38 @@ import React from 'react';
 // import  ReactDOM  from 'react-dom';
 import {createRoot} from 'react-dom/client'
 
-class APP extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
+class App extends React.Component {
+    constructor(props) {
+      super(props);
 
-    return <div>Latitude: </div>;
+      this.state = { lat: null, errorMessage: '' };
+
+      window.navigator.geolocation.getCurrentPosition(
+        position => {
+          // we called setstate!!!
+          this.setState({ lat: position.coords.latitude });
+        },
+        err => {
+          this.setState({ errorMessage: err.message });
+        }
+      );
+    }
+
+  // React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage} </div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat){
+      return <div>latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
 // ReactDOM.render(<App />, document.querySelector('#root'));
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<APP />)
+root.render(<App />)
