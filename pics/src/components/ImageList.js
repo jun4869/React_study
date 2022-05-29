9ac7e4be7ffa13cddd8280f88ff9fc275157866f
jun4869 <jun2194k@gmail.com>
2022-05-29 22:04:00 +0900

    seciton9

---
+++ pics/src/components/ImageList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ImageList = props => {
+  return props.images.map(({ description, id, urls }) => {
+    return <img alt={description} key={id} src={urls.regular} />;
+  });
+
+};
+
+export default ImageList;
